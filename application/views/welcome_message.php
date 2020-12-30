f30a98c0bd7d77d49282d59b202cf5a44fe46bb8
hapisd <hapisd@gmail.com>
2020-12-30 21:14:25 +0700

    navbar

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -13,28 +13,28 @@
   </head>
   
   <body>
-    <div class="sticky-top" style="background-image: url(asset/image/header.png);background-size: cover;height: 250px">
+    <div id="container" class="sticky-top" style="background-image: url(asset/image/header.png);">
       <nav class="navbar navbar-expand-lg navbar-light" >
         <div class="container-fluid">
           <a class="navbar-brand" href="#" style="margin-left: 4rem">
             <img src="asset/image/Marhara.png" alt="">
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
           </button>
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-1 mb-lg-0" style="padding-left: 24%">
+            <ul class="navbar-nav me-auto mb-1 mb-lg-0">
               <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" aria-current="page" href="#">Home</a>
               </li>
 
               <li class="nav-item">
-                  <a class="nav-link" href="#">Discover</a>
+                <a class="nav-link" href="#">Discover</a>
               </li>
               
-            <li class="nav-item">
-                  <a class="nav-link" href="#">Profile</a>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Profile</a>
               </li>
             </ul>
           </div>
