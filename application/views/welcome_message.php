1da1c2d4685b314d617418733ec09d0071d84253
hapisd <hapisd@gmail.com>
2020-12-30 09:50:24 +0700

    navbar

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -13,7 +13,8 @@
   </head>
   
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+    <div class="sticky-top" style="background-image: url(asset/image/header.png);background-size: cover;height: 250px">
+      <nav class="navbar navbar-expand-lg navbar-light" >
         <div class="container-fluid">
           <a class="navbar-brand" href="#" style="margin-left: 4rem">
             <img src="asset/image/Marhara.png" alt="">
@@ -23,22 +24,23 @@
           </button>
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-1 mb-lg-0" style="padding-left: 24%">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Discover</a>
-                </li>
-                
+            <ul class="navbar-nav me-auto mb-1 mb-lg-0" style="padding-left: 24%">
               <li class="nav-item">
-                    <a class="nav-link" href="#">Profile</a>
-                </li>
-          </ul>
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Discover</a>
+              </li>
+              
+            <li class="nav-item">
+                  <a class="nav-link" href="#">Profile</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        </div>
-    </nav>
+      </nav>
+    </div>
 
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
