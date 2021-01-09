7d98315ab6b2326644785d4a11243b5511f32b55
hapisd <hapisd@gmail.com>
2021-01-09 10:43:08 +0700

    Revert "Update welcome_message.php"
    This reverts commit 79601c570ce3a9320f092c101aa8e05c3d3c924d.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -31,7 +31,7 @@
                 </li>
 
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Discovery</a>
+                  <a class="nav-link" href="#">Discover</a>
                 </li>
                 
                 <li class="nav-item">
