79601c570ce3a9320f092c101aa8e05c3d3c924d
hapisd <hapisd@gmail.com>
2021-01-09 10:38:08 +0700

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -31,7 +31,7 @@
                 </li>
 
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Discover</a>
+                  <a class="nav-link" href="#">Discovery</a>
                 </li>
                 
                 <li class="nav-item">
