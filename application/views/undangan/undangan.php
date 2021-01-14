efa78a4b66c35c5440d0d6ab7a17f5f50714a783
hapisd <hapisd@gmail.com>
2021-01-15 03:04:27 +0700

    z

--- application/views/undangan/undangan.php
+++ application/views/undangan/undangan.php
@@ -14,21 +14,21 @@
     <title>couple name</title>
   </head>
   <body>
-    <div id="konten">
-        <div id="about" style="min-height: 640px;">
+    <div id="container">
+        <div id="about" style="position: relative; height: 640px; overflow: hidden;">
             <img src="<?= base_url()?>assets/image/bungaatas.png" style="position: absolute;transform: translate(-50%); left: 50%; top: -60px; z-index: 2">
             <p style="position: absolute; transform: translate(-50%); left: 50%; top: 50px; z-index: 2">about us</p>
             <h3 style="position: absolute; transform: translate(-50%); left: 50%; top: 80px; z-index: 2; text-align: center;">bride and groom</h3>
             <img src="<?= base_url()?>assets/image/bungakanan.png" style="position: absolute; right: 0; top: 148px; z-index: 2">
             <img src="<?= base_url()?>assets/image/bungakiri.png" style="position: absolute; left: 0; top: 347px; z-index: 2">
-            <img src="<?= base_url()?>assets/image/bungabawah.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -80px; z-index: 2">
-            <img src="<?= base_url()?>assets/image/bgwarna.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -150px; z-index: 1; width: 150%; max-height: 100%">
-            <img src="<?= base_url()?>assets/image/detail.png" style="position: absolute; transform: translate(-50%); left: 50%; top: 100px; z-index: 2; max-width: 80%; max-height: 80%">
+            <img src="<?= base_url()?>assets/image/bungabawah.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -100px; z-index: 2">
+            <img src="<?= base_url()?>assets/image/bgwarna.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -100px; z-index: 1; width: 150%; max-height: 100%">
+            <img src="<?= base_url()?>assets/image/detail.png" style="position: absolute; transform: translate(-50%); left: 50%; top: 20%; z-index: 2;">
         </div><br><br><br>
 
-        <div id="galeri" style="min-height: 640px">
-            <p style="position: absolute; transform: translate(-50%); left: 50%; top: 620px; z-index: 2">pray for us</p>
-            <img src="<?= base_url()?>assets/image/bgwarna2.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -700px; z-index: 1; width: 100%; max-height: 100%">
+        <div id="galeri" style="position: relative; height: 500px; overflow: hidden;">
+            <p style="position: absolute; transform: translate(-50%); left: 50%; z-index: 2">pray for us</p>
+            <img src="<?= base_url()?>assets/image/bgwarna2.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -108px; z-index: 1; width: 150%; max-height: 100%">
             <div class="swiper-container">
                 <div class="swiper-wrapper">
                   <div class="swiper-slide" style="background-image:url(./images/nature-1.jpg)"></div>
@@ -45,8 +45,18 @@
             </div>            
         </div>
 
-        <div>
-            
+        <div id="tanggalan" style="position: relative; height: 640px; overflow: hidden;">
+            <img src="<?= base_url()?>assets/image/bgwarna3.png" style="position: absolute; transform: translate(-50%); left: 50%; top: -140px; z-index: 1; width: 150%; max-height: 100%">
+            <p style="position: absolute; transform: translate(-50%); left: 50%; z-index: 2">save the date</p>
+            <img src="<?= base_url()?>assets/image/tanggalan.png" style="position: absolute; transform: translate(-50%); left: 50%; top: 100px; z-index: 1;">
+            <img src="<?= base_url()?>assets/image/bungabawah2.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -90px; z-index: 2;">
+        </div>
+
+        <div id="lokasi" style="position: relative; height: 640px; overflow: hidden;">
+            <p style="position: absolute; transform: translate(-50%); left: 50%; z-index: 2; top: 100px">event location</p>
+            <img src="<?= base_url()?>assets/image/bungaatas4.png" style="position: absolute; transform: translate(-50%); left: 50%; top: -33px; z-index: 1;">
+            <img src="<?= base_url()?>assets/image/bungabawah4.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -50px; z-index: 1;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.1175483303145!2d106.82756731347143!3d-6.378824395384294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec0869e31b4f%3A0xaa40278d69385917!2sHotel%20Bumi%20Wiyata!5e0!3m2!1sen!2sid!4v1610653900642!5m2!1sen!2sid" width="264" height="264" frameborder="0" style="border:0; position: absolute; transform: translate(-50%); left: 50%; top: 220px" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </div>
 
     </div>
