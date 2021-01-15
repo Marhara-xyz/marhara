de9d17932440160c0b4eb6450ada73f3b9246577
hapisd <hapisd@gmail.com>
2021-01-16 04:59:21 +0700

    form

--- application/views/undangan/undangan.php
+++ application/views/undangan/undangan.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -9,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-    <link rel="stylesheet" href="<?= base_url()?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url()?>assets/css/style2.css">
 
     <title>couple name</title>
   </head>
@@ -50,6 +51,28 @@
             <p style="position: absolute; transform: translate(-50%); left: 50%; z-index: 2">save the date</p>
             <img src="<?= base_url()?>assets/image/tanggalan.png" style="position: absolute; transform: translate(-50%); left: 50%; top: 100px; z-index: 1;">
             <img src="<?= base_url()?>assets/image/bungabawah2.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -90px; z-index: 2;">
+            <div style="position: absolute; transform: translate(-50%); left: 50%; top: 220px; z-index: 2">
+            
+                <h1 style="text-align: center;">Countdown Clock</h1>
+                <div id="clockdiv">
+                  <div>
+                    <span id="days"></span>
+                    <div class="smalltext">Days</div>
+                  </div>
+                  <div>
+                    <span id="hours"></span>
+                    <div class="smalltext">Hours</div>
+                  </div>
+                  <div>
+                    <span id="minutes"></span>
+                    <div class="smalltext">Minutes</div>
+                  </div>
+                  <div>
+                    <span id="seconds"></span>
+                    <div class="smalltext">Seconds</div>
+                  </div>
+                </div>
+            </div>
         </div>
 
         <div id="lokasi" style="position: relative; height: 640px; overflow: hidden;">
@@ -57,6 +80,44 @@
             <img src="<?= base_url()?>assets/image/bungaatas4.png" style="position: absolute; transform: translate(-50%); left: 50%; top: -33px; z-index: 1;">
             <img src="<?= base_url()?>assets/image/bungabawah4.png" style="position: absolute; transform: translate(-50%); left: 50%; bottom: -50px; z-index: 1;">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.1175483303145!2d106.82756731347143!3d-6.378824395384294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec0869e31b4f%3A0xaa40278d69385917!2sHotel%20Bumi%20Wiyata!5e0!3m2!1sen!2sid!4v1610653900642!5m2!1sen!2sid" width="264" height="264" frameborder="0" style="border:0; position: absolute; transform: translate(-50%); left: 50%; top: 220px" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+
+        <div id="rsvp" style="position: relative; height: 640px; overflow: hidden;">
+            <form style="position: absolute; transform: translate(-50%); left: 50%; z-index: 2; top: 100px">
+
+                <div class="element-form">
+                <label>Nama</label><br>
+                <span class="field" input type="text" name="nama" /></span>
+                </div>
+            
+
+                <div class="element-form">
+                    <label>Alamat</label><br>
+                    <span class="field"><textarea name="alamat"></textarea></span>
+                </div>
+
+                <div class="pilihan">
+                    <input type="radio" id="ya" name="keterangan" value="1">
+                    <label for="ya">Pasti</label><br>
+                </div>
+                <div class="pilihan">
+                    <input type="radio" id="tidak" name="keterangan" value="2">
+                    <label for="tidak">Kaga Dah</label><br>
+                </div>
+                <div class="pilihan">
+                    <input type="radio" id="mungkin" name="keterangan" value="3">
+                    <label for="mungkin">Maybe Yes Maybe No</label><br>
+                </div>
+
+                <div class="element-form">
+                    <label>Ucapan</label><br>
+                    <span><textarea name="ucapan"></textarea></span>
+                </div>
+
+                <div class="element-form">
+                    <span class="fiel"><input type="submit" /></span>
+                </div>
+            </form>
         </div>
 
     </div>
@@ -86,7 +147,38 @@
       },
     });
     </script>
+    <script>
+        // Set the date we're counting down to
+        var countDownDate = new Date("Jan 19, 2021 15:37:25").getTime();
 
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+
+          // Get today's date and time
+          var now = new Date().getTime();
+            
+          // Find the distance between now and the count down date
+          var distance = countDownDate - now;
+            
+          // Time calculations for days, hours, minutes and seconds
+          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+          // Output the result in an element with id="demo"
+          document.getElementById("days").innerHTML = ('0' + days).slice(-2);
+          document.getElementById("hours").innerHTML = ('0' + hours).slice(-2);
+          document.getElementById("minutes").innerHTML = ('0' + minutes).slice(-2);
+          document.getElementById("seconds").innerHTML = ('0' + seconds).slice(-2);
+            
+          // If the count down is over, write some text 
+          if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("clockdiv").innerHTML = "EXPIRED";
+          }
+        }, 1000);
+    </script>
     <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
